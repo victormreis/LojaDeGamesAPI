9cf99bb02a7c5fa3341bd2c93a688a236460a567
Victor Martins <vitor_jhoul@hotmail.com>
2022-03-31 09:28:24 -0300

    Criando o projeto

---
+++ README.md
@@ -0,0 +1,3 @@
+# Game Store - API
+
+* API desenvolvida utilizando Spring durante o bootcamp da Generation Brasil para sintetizar o aprendizado.
---
+++ games-api/src/test/java/org/generation/games/GamesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
